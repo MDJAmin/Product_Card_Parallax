05223a9903c7a7f607bd039ba55040ee548d493e
MDJAmin <mohammadamindf@gmail.com>
2024-10-09 19:19:55 +0330

    Feat : complete the JS functionality
    and add ease property to my animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-"use strict"
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modeSwitch = document.querySelector(".mode-switch");
+
+  modeSwitch.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+    modeSwitch.classList.toggle("active");
+  });
+});
+
+
+VanillaTilt.init(document.querySelectorAll(".box"),{
+    max:15,
+    speed:300,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    scale: 1.05,
+})
+
+
